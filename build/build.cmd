e7cd59d370c5679d0d31b3b442c02f9156b7dfbe
超神的冰凉 <2801621420@qq.com>
2023-01-25 23:45:51 +0800

    使用bash运行构建脚本

--- build/build.cmd
+++ build/build.cmd
@@ -2,5 +2,5 @@
 @REM Run build.sh with Git-Bash on Windows.
 set git_path=C:\Program Files\Git\cmd\git.exe
 for /f "delims=" %%F in ('where git') do set git_path=%%F
-set git_path=%git_path:~0,-12%\bin\sh.exe
+set git_path=%git_path:~0,-12%\bin\bash.exe
 %git_path% %~dp0"/build.sh"
